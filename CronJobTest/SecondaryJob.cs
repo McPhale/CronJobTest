3508ebb6f9f5e49414c5187cb06891f4a64a8012
Michael Jantz <Michael.Jantz@avisbudget.com>
2019-03-11 11:57:06 -0500

    See last commit.

---
+++ CronJobTest/SecondaryJob.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace CronJobTest
+{
+    //[DisallowConcurrentExecution]
+    class SecondaryJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Console.Out.WriteLineAsync($"{DateTime.Now} Hello from Secondary job!");
+        }
+    }
+}
---
+++ CronJobTest/SecondaryJobScheduler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+
+namespace CronJobTest
+{
+    public class SecondaryJobScheduler
+    {
+        public static async Task StartAsync()
+        {
+
+            StdSchedulerFactory factory = new StdSchedulerFactory();
+            IScheduler scheduler = await factory.GetScheduler();
+            await scheduler.Start();
+
+            IJobDetail SecondaryJob = JobBuilder.Create<SecondaryJob>().Build();
+
+            ITrigger SecondaryJobTrigger = TriggerBuilder.Create()
+            .WithIdentity("SecondaryJobTrigger")
+            .StartNow()
+            //.WithCronSchedule("0 0/5 * 1/1 * ? *")
+            .WithCronSchedule("0 0 0 1 1 ? 2099")
+            .Build();
+
+            await scheduler.ScheduleJob(SecondaryJob, SecondaryJobTrigger);   
+
+        }  
+    }
+}
